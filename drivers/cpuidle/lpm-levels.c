8787d828790128d6d38da5449b0e653322b37fd8
Murali Nalajala <mnalajal@codeaurora.org>
2015-01-07 19:36:57 -0800

    cpu_pm: Add level to the cluster pm notification
    Cluster pm notifications without level information increases difficulty
    and complexity for the registered drivers to figure out when the last
    coherency level is going into power collapse.
    
    Send notifications with level information that allows the registered
    drivers to easily determine the cluster level that is going in/out of
    power collapse.
    
    There is an issue with this implementation. GIC driver saves and
    restores the distributed registers as part of cluster notifications. On
    newer platforms there are multiple cluster levels are defined (e.g l2,
    cci etc). These cluster level notofications can happen independently.
    On MSM platforms GIC is still active while the cluster sleeps in idle,
    causing the GIC state to be overwritten with an incorrect previous state
    of the interrupts. This leads to a system hang. Do not save and restore
    on any L2 and higher cache coherency level sleep entry and exit.
    
    Change-Id: I31918d6383f19e80fe3b064cfaf0b55e16b97eb6
    Signed-off-by: Archana Sathyakumar <asathyak@codeaurora.org>
    Signed-off-by: Murali Nalajala <mnalajal@codeaurora.org>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -26,6 +26,7 @@
 #include <linux/suspend.h>
 #include <linux/pm_qos.h>
 #include <linux/quickwakeup.h>
+#include <linux/cpu_pm.h>
 #include <linux/of_platform.h>
 #include <linux/smp.h>
 #include <linux/remote_spinlock.h>
@@ -487,9 +488,11 @@ static int cluster_configure(struct lpm_cluster *cluster, int idx,
 		if (ret)
 			goto failed_set_mode;
 
+		/*
+		 * Notify that the cluster is entering a low power mode
+		 */
 		if (level->mode[i] == MSM_SPM_MODE_POWER_COLLAPSE)
 			cpu_cluster_pm_enter(cluster->aff_level);
-
 	}
 	if (level->notify_rpm) {
 		struct cpumask nextcpu, *cpumask;
@@ -633,7 +636,6 @@ static void cluster_unprepare(struct lpm_cluster *cluster,
 		if (cluster->levels[last_level].mode[i] ==
 				MSM_SPM_MODE_POWER_COLLAPSE)
 			cpu_cluster_pm_exit(cluster->aff_level);
-
 	}
 unlock_return:
 	spin_unlock(&cluster->sync_lock);

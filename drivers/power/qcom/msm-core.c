fdbd1ec4350e445d65f2aca512d2a3b71512f10b
Archana Sathyakumar <asathyak@codeaurora.org>
2016-09-13 14:36:04 -0600

    power: qcom: ea: Fix uninitialized temperature and cpu variable
    In update_userspace_power, incorrect cpumask could cause the function to
    use uninitialized cpu variable. Initialize it to -1 at the beginning of
    the function and treat unmodified value as error condition.
    
    Change-Id: Ieccdc3f54f9c9f2cecc6b8578400c6fe44333177
    Signed-off-by: Archana Sathyakumar <asathyak@codeaurora.org>
    Signed-off-by: Jake Weinstein <xboxlover360@gmail.com>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- drivers/power/qcom/msm-core.c
+++ drivers/power/qcom/msm-core.c
@@ -309,7 +309,7 @@ static int update_userspace_power(struct sched_params __user *argp)
 {
 	int i;
 	int ret;
-	int cpu;
+	int cpu = -1;
 	struct cpu_activity_info *node;
 	struct cpu_static_info *sp, *clear_sp;
 	int cpumask, cluster, mpidr;
@@ -332,7 +332,7 @@ static int update_userspace_power(struct sched_params __user *argp)
 		}
 	}
 
-	if (cpu >= num_possible_cpus())
+	if ((cpu < 0) || (cpu >= num_possible_cpus()))
 		return -EINVAL;
 
 	node = &activity[cpu];

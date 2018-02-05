51f29729ef1ffa1e3a2c6290a1bd9b87160db6ba
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2018-02-05 15:25:43 +0000

    cpuidle: remove undefined struct member

--- drivers/cpuidle/governors/menu.c
+++ drivers/cpuidle/governors/menu.c
@@ -292,7 +292,6 @@ static int menu_select(struct cpuidle_driver *drv, struct cpuidle_device *dev)
 	}
 
 	data->last_state_idx = CPUIDLE_DRIVER_STATE_START - 1;
-	data->exit_us = 0;
 
 	/* Special case when user has set very strict latency requirement */
 	if (unlikely(latency_req == 0))

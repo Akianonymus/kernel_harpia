389597e35f3c0cee6f9edda7712e2f2d27d28dd4
Chris Renshaw <osm0sis@outlook.com>
2018-05-22 22:25:13 +0530

    Add Anykernel2 support
    Signed-off-by: Akianonymus <anonymus.aki@gmail.com>

---
+++ Anykernel2/harpia/anykernel.sh
@@ -0,0 +1,46 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() {
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=Moto g play
+device.name2=harpia
+device.name3=Moto G4 Play
+device.name4=Mota G play
+device.name5=Mutu G4 play
+} # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
+
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+#insert_line init.qcom.rc "init.bleedingedge.rc" after "import init.target.rc" "import /init.bleedingedge.rc"
+
+# end ramdisk changes
+
+write_boot;
+
+## end install

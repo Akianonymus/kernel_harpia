8e2ddc1baf92792e91199f0c56513121a83b726c
Akianonymus <anonymus.aki@gmail.com>
2018-05-22 09:26:52 +0530

    cpufreq : Impulse :drop cpufreq_put_global_kobject(); function calls
    Upstream commit ade389026b4fc7aef2ccd47e4147cc3cabe11215 dropped this funtion to fix uevent broken in cpufreq driver.
    
    so drop this depracated function call which otherwise result in following compilation error
    
    drivers/cpufreq/cpufreq_impulse.c:12:
    drivers/cpufreq/cpufreq_impulse.c: In function ‘cpufreq_governor_impulse’:
    drivers/cpufreq/cpufreq_impulse.c:1224:12: error:
    implicit declaration of function
    ‘cpufreq_get_global_kobject’; did you mean
    ‘cpufreq_get_policy’?
    [-Werror=implicit-function-declaration]
        WARN_ON(cpufreq_get_global_kobject());
                ^ include/asm-generic/bug.h:86:25: note: in
    definition of macro ‘WARN_ON’
      int __ret_warn_on = !!(condition); \
                             ^~~~~~~~~
    drivers/cpufreq/cpufreq_impulse.c:1235:5: error: implicit
    declaration of function ‘cpufreq_put_global_kobject’; did
    you mean ‘cpufreq_get_policy’?
    [-Werror=implicit-function-declaration]
         cpufreq_put_global_kobject();
         ^~~~~~~~~~~~~~~~~~~~~~~~~~
         cpufreq_get_policy
    
    Signed-off-by: Akianonymus <anonymus.aki@gmail.com>

--- drivers/cpufreq/cpufreq_impulse.c
+++ drivers/cpufreq/cpufreq_impulse.c
@@ -1221,7 +1221,6 @@ static int cpufreq_governor_impulse(struct cpufreq_policy *policy,
 		tunables->usage_count = 1;
 		policy->governor_data = tunables;
 		if (!have_governor_per_policy()) {
-			WARN_ON(cpufreq_get_global_kobject());
 			common_tunables = tunables;
 		}
 
@@ -1232,7 +1231,6 @@ static int cpufreq_governor_impulse(struct cpufreq_policy *policy,
 			policy->governor_data = NULL;
 			if (!have_governor_per_policy()) {
 				common_tunables = NULL;
-				cpufreq_put_global_kobject();
 			}
 			return rc;
 		}
@@ -1256,8 +1254,6 @@ static int cpufreq_governor_impulse(struct cpufreq_policy *policy,
 
 			sysfs_remove_group(get_governor_parent_kobj(policy),
 					get_sysfs_attr());
-			if (!have_governor_per_policy())
-				cpufreq_put_global_kobject();
 			common_tunables = NULL;
 		}
 
